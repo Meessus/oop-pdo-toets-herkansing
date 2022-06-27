1ab9e83630fb1774a1c7b6315c8307c3ea13c65c
Mees <334104@student.mboutrecht.nl>
2022-06-27 11:59:50 +0200

    Final commit: Video + Database

--- richestpeople/index.php
+++ richestpeople/index.php
@@ -1,5 +1,8 @@
 <?php
-require './Database.php';
+require '../functions.php';
+
+$db  = new Database();
+
 ?>
 
 
@@ -9,7 +12,7 @@ require './Database.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BE PDO Herkansing</title>
+    <title>BE PDO herkansing</title>
 </head>
 <body>
 <h2> De vijf rijkste mensen ter wereld</h2>
